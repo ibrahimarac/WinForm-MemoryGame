69748c7142adfe7d865bfca9c8fbf308b53cfe53
Halil İbrahim ARAÇ <iboarac@gmail.com>
2021-06-16 18:36:19 +0300

    Kaynak kodlar yüklendi

---
+++ MemoryGame/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Ibrahim.MemoryGame.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ibrahim.MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, GameObject> gameObjects = new Dictionary<int, GameObject>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        void AddControlToFlowLayout(FlowLayoutPanel panel, UserControl ctl)
+        {
+            if (flowContainer.InvokeRequired)
+            {
+                flowContainer.Invoke(new Action<FlowLayoutPanel, UserControl>(AddControlToFlowLayout), flowContainer, ctl);
+            }
+            panel.Controls.Add(ctl);
+        }
+
+        Task CreateGameArea()
+        {
+            return Task.Run(() =>
+            {
+                /*
+                    Önce bir dictionary içerisine tüm elemanları yükleyelim. 
+                */
+                for (int i = 1; i <= 20; i++)
+                {
+                    int imageNumber = (i == 20 || i == 10 ? 10 : i % 10);
+                    gameObjects.Add(i, new GameObject
+                    {
+                        NormalImage = Resources._0,
+                        OpenImage = (Bitmap)
+                            Resources.ResourceManager.GetObject("_" + imageNumber),
+                        Id = i,
+                        ImageNumber=imageNumber
+                    });                    
+                }
+            }).ContinueWith((t)=> {
+                Parallel.For(1, 21, (i) =>
+                {
+                    AddControlToFlowLayout(flowContainer, gameObjects[i]);
+                });
+            }).ContinueWith((t)=> {
+                for (int i = 1; i <=20 ; i++)
+                {
+                    gameObjects[i].Click += GameObjectClick;
+                }
+            });
+        }
+
+        GameObject GetPartner(GameObject obj)
+        {
+            GameObject returnObj = null;
+            for (int i = 1; i <= 20; i++)
+            {
+                if(gameObjects[i].ObjectState==State.Open 
+                    && 
+                    gameObjects[i].Id!=obj.Id)
+                {
+                    returnObj = gameObjects[i];
+                }
+            }
+            return returnObj;
+        }
+
+        private void GameObjectClick(object sender, EventArgs e)
+        {
+            GameObject obj = sender as GameObject;
+
+            if (obj.ObjectState != State.Normal)
+                return;
+
+            GameObject partner = GetPartner(obj);
+            obj.ObjectState = State.Open;
+            if (partner!=null)
+            {
+                if (partner.ImageNumber == obj.ImageNumber)
+                {
+                    partner.ObjectState = State.Matched;
+                    obj.ObjectState = State.Matched;
+                }
+                else
+                {
+                    partner.ObjectState = State.Normal;
+                }
+                partner.Animate();
+            }
+            obj.Animate();
+            
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await CreateGameArea();
+        }
+    }
+}
+
---
+++ MemoryGame/GameObject.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+namespace Ibrahim.MemoryGame
+{
+    public enum State
+    {
+        Normal,
+        Open,
+        Matched
+    }
+    public partial class GameObject : UserControl
+    {
+        Bitmap openImage;
+        Bitmap normalImage;
+        int id;
+        State objectState,previousState;
+
+        public GameObject()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            ObjectState = State.Normal;
+            PreviousState = State.Normal;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public int ImageNumber { get; set; }
+
+        public Bitmap OpenImage
+        {
+            get
+            {
+                return openImage;
+            }
+            set
+            {
+                openImage = value;
+            }
+        }
+
+        public Bitmap NormalImage
+        {
+            get
+            {
+                return normalImage;
+            }
+            set
+            {
+                normalImage = value;
+                this.BackgroundImage = value;
+            }
+        }
+              
+        public State ObjectState
+        {
+            get
+            {
+                return objectState;
+            }
+            set
+            {
+                PreviousState = objectState;
+                objectState = value;
+            }
+        }
+
+        public State PreviousState
+        {
+            get
+            {
+                return previousState;
+            }
+            set
+            {
+                previousState = value;
+            }
+        }
+       
+        void ChangeImage(Image imgSource,Image imgDestination)
+        {
+            Task.Run(() =>
+            {
+                for (float i = 1; i >=0; i-=0.1F)
+                {
+                    this.BackgroundImage=SetImageOpacity(imgSource, i);
+                    Task.Delay(50).Wait();
+                }
+            }).ContinueWith(t=> {
+                if (imgDestination != null)
+                {
+                    for (float i = 0; i <= 1; i += 0.1F)
+                    {
+                        this.BackgroundImage = SetImageOpacity(imgDestination, i);
+                        Task.Delay(50).Wait();
+                    }
+                }                
+            });
+        }
+        
+        Image SetImageOpacity(Image image, float opacity)
+        {
+            //create a Bitmap the size of the image provided  
+            Bitmap bmp = new Bitmap(image.Width, image.Height);
+            //create a graphics object from the image  
+            using (Graphics gfx = Graphics.FromImage(bmp))
+            {
+                //create a color matrix object  
+                ColorMatrix matrix = new ColorMatrix();
+                //set the opacity  
+                matrix.Matrix33 = opacity;
+                //create image attributes  
+                ImageAttributes attributes = new ImageAttributes();
+                //set the color(opacity) of the image  
+                attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+                //now draw the image  
+                gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+            }
+            return bmp;
+        }
+
+        public void Animate()
+        {
+            if (ObjectState == State.Normal && PreviousState == State.Open)
+            {
+                ChangeImage(OpenImage, NormalImage);
+            }
+            else if (ObjectState == State.Open && PreviousState == State.Normal)
+            {
+                ChangeImage(NormalImage, OpenImage);
+            }
+            else if (ObjectState == State.Matched && PreviousState == State.Open)
+            {
+                ChangeImage(OpenImage, null);
+            }
+        }
+
+    }
+}
+
